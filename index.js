b4e59b1b28f4da550db2fc00e48ad0c77ec3b97f
Brianna Townsed <b.townsend42@yahoo.com>
2023-07-02 17:31:32 -0500

    SaturdayFunMondayWork

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// code your solution here
+// index.js
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// Testing the function
+console.log(saturdayFun()); // Output: "This Saturday, I want to roller-skate!"
+console.log(saturdayFun("go hiking")); // Output: "This Saturday, I want to go hiking!"
+// index.js
+
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+// Testing the function
+console.log(mondayWork()); // Output: "This Monday, I will go to the office."
+console.log(mondayWork("attend a meeting")); // Output: "This Monday, I will attend a meeting."
+// index.js
+
+function wrapAdjective(flair = "*") {
+  return function (adjective) {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+
+// Testing the function
+const highlight1 = wrapAdjective("*");
+console.log(highlight1("awesome")); // Output: "You are *awesome*!"
+
+const highlight2 = wrapAdjective("||");
+console.log(highlight2("amazing")); // Output: "You are ||amazing||!"
